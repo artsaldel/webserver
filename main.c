7cea24a20cc719ae605d7163a5768e265e8a853f
Arturo <artsaldel@hotmail.com>
2016-08-30 20:07:53 -0600

    First commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: arturo
+ *
+ * Created on 30 de agosto de 2016, 07:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
